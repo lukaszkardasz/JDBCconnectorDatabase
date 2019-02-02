8d95ddb16c033825840a985a223e84f7f7383906
[n2god] <[n2god@o2.pl]>
2019-02-02 13:55:01 +0100

    add configuration application.properties on resources - database connection

---
+++ src/main/java/SpringDataJPA/SpringApp.java
@@ -0,0 +1,17 @@
+package SpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+class SpringApp {
+
+    public static void main(String[] args) {
+    ConfigurableApplicationContext context = SpringApplication
+            .run(SpringApp.class, args);
+
+
+
+    }
+}
